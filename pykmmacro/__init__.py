f827fa06cb138f202827314400e4d3f66d034fbf
Seaoak <seaoak@seaoak.jp>
2025-05-02 10:35:01 +0900

    Add "import" from the submodule "utils"

--- pykmmacro/__init__.py
+++ pykmmacro/__init__.py
@@ -1,2 +1,3 @@
 from .keyboardinput import key_press, MODIFIER, NORMAL_KEY
+from .utils import *
 from .windowsapi import get_active_window_info, get_screen_info, OffsetInScreen, OffsetInWindow, PositionInScreen
