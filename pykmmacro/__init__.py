a8c2b6a4c7e6bae3bb798e373d70113ca73b4bf1
Seaoak <seaoak@seaoak.jp>
2025-05-06 21:03:41 +0900

    Implement mouse listener (add a new function "setup_mouse_listener()")

--- pykmmacro/__init__.py
+++ pykmmacro/__init__.py
@@ -2,7 +2,7 @@ from .clipboard import copy_to_clipboard
 from .keyboardinput import key_press, MODIFIER, NormalKey
 from .keyboardstat import ModifierKey, setup_keyboard_listener
 from .mouseinput import MOUSE_BUTTON, mouse_click, mouse_move_relative, mouse_move_to
-from .mousestat import get_mouse_position
+from .mousestat import get_mouse_position, setup_mouse_listener
 from .screenshot import Color, Screenshot
 from .utils import *
 from .windowsapi import activate_window, get_active_window_info, get_screen_info, OffsetInScreen, OffsetInWindow, PositionInScreen, show_dialog
--- pykmmacro/mousestat.py
+++ pykmmacro/mousestat.py
@@ -1,7 +1,43 @@
+from collections import deque
+
 import pydirectinput
+from pynput import mouse
 
 from .windowsapi import PositionInScreen
+
+#=============================================================================
+# Get mouse cursor position
 
 def get_mouse_position() -> PositionInScreen:
     x, y = pydirectinput.position()
     return PositionInScreen(x, y)
+
+#=============================================================================
+# Mouse event listener
+# https://pypi.org/project/pynput/
+
+def setup_mouse_listener():
+    # https://docs.python.org/3/library/collections.html#deque-objects
+    # - deque is thread-safe.
+    # - if deque is full, when a new item is added, the item at the opposite side will be discarded.
+    queue: deque[PositionInScreen] = deque(maxlen=1)
+
+    def get_position_of_latest_click() -> PositionInScreen | None:
+        try:
+            pos = queue.popleft()
+            return pos
+        except IndexError:
+            return None
+
+    def on_click(x, y, button, pressed, _injected):
+        if button != mouse.Button.left:
+            return
+        if pressed:
+            return # ignore 'mouseDown' event (use 'mouseUp' event)
+        queue.append(PositionInScreen(x, y))
+
+    listener = mouse.Listener(on_click=on_click)
+
+    listener.start() # start new listener theread
+
+    return get_position_of_latest_click
