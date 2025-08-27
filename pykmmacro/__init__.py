d7201ffa38b8cfa50c4cf16b2d27ddee1c8e98eb
Seaoak <seaoak@seaoak.jp>
2025-08-27 11:46:42 +0900

    Fix a lack of an import item

--- pykmmacro/__init__.py
+++ pykmmacro/__init__.py
@@ -1,6 +1,6 @@
  # type: ignore
 from .clipboard import copy_to_clipboard
-from .keyboardinput import key_press, NormalKey
+from .keyboardinput import key_press, NormalKey, with_modifier_keys
 from .keyboardstat import setup_keyboard_listener
 from .modifier import MyModifier
 from .mouseinput import MouseButton, mouse_click, mouse_move_relative, mouse_move_to
